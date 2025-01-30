179c80e0ddcd58e376e8514530eb2b1aa34c5dd7
CristianAlvarez <cristianandres1496@gmail.com>
2025-01-30 06:08:57 -0500

    updating a home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,18 +3,22 @@ import React from "react";
 const Home = () => {
   return (
     <div className="h-[calc(100vh-104px)] mt-12">
-      <div className="  ">
+      <div className=" ">
         <div className="m-auto flex justify-center items-center w-[78%] ">
           <img className="w-2xl" src="./img1.png" alt="" />
-          <p className="font-thin text-6xl">
-            Write your amazing notes with our app!
-          </p>
-          <a
-            href="/write"
-            className="px-5 py-3 rounded-3xl bg-violet-500 text-white"
-          >
-            Write!
-          </a>
+          <div>
+            <p className="font-thin text-5xl leading-[65px]">
+              Create your amazing notes with{" "}
+              <strong className="text-purple-700 font-semibold">WRITE</strong>{" "}
+              app!
+            </p>
+            <a
+              href="/write"
+              className="block w-max my-5 px-5 py-3 rounded-3xl bg-violet-500 text-white"
+            >
+              Write!
+            </a>
+          </div>
         </div>
       </div>
     </div>
